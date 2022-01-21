ade7ae770756179cd26afa091ceec6c35e3ede9f
Deepank21103075 <96749780+Deepank21103075@users.noreply.github.com>
2022-01-21 17:20:15 +0530

    Add files via upload

---
+++ O_A-2_CSE_21103075.py
@@ -0,0 +1,48 @@
+Output-1
+
+a-part
+THE LENGTH OF STRING IS 35
+b-part
+THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+e-part
+THE INDEX OF SUBSTRING a is  10
+f-part
+THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+Output-2
+
+ENTER YOUR NAME: Deepank
+ENTER YOUR SID: 21103075
+ENTER YOUR DEPARTMENT: CSE
+ENTER YOUR CGPA: 10
+Hey Deepank, Here!
+My SID is 21103075
+I am from CSE and my CGPA is 10.000000
+
+Output-3
+
+a-part
+8
+b-part
+58
+c-part
+50
+
+Output-4
+
+Enter first number: 7
+Enter second number: 9
+Enter third number: 8
+THE LARGEST NUMBER IS  9
+
+Output-5
+
+ENTER A STRING: Namikaze 
+No
+
+Output-6
+
+ENTER FIRST SIDE OF TRIANGLE: 6
+ENTER SECOND SIDE OF TRIANGLE: 8
+ENTER THIRD SIDE OF TRIANGLE: 10
+Yes
